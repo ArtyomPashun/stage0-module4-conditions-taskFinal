3771a6c9d68f82576b1910500cfdfe0113dd0ad6
Artsiom Pashun <a.pashun@syberry.com>
2023-04-17 10:03:12 +0200

    Finished module 4, final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else if (month == 2 && year % 4 == 0) {
+            if (year % 100 == 0 && year % 400 != 0) {
+                System.out.println("28");
+            } else {
+                System.out.println("29");
+            }
+        } else if (month == 2) {
+            System.out.println("28");
+        } else if (month == 4 || month == 6 || month == 9 || month == 10) {
+            System.out.println("30");
+        } else {
+            System.out.println("31");
+        }
     }
 }
